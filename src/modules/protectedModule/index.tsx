5ffa4287b6a0a20bebd3781c7400f4652c5b3221
ElielC <eliel-calebi@hotmail.com>
2021-06-01 10:21:41 -0300

    add redirect protected module

--- src/modules/protectedModule/index.tsx
+++ src/modules/protectedModule/index.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { useRouter } from 'next/router'
+
 import useAuthContext from '@/hooks/useAuthContext'
 import Login from '@/pages'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// this module returns the login component if the user is not logged in
 const ProtectedModule = Component => {
-  const Auth = props => {
-    const { isSigned } = useAuthContext()
+  const { isSigned } = useAuthContext()
 
+  const Auth = props => {
     if (isSigned === null) {
       return <></>
     } else if (!isSigned) {
@@ -14,6 +17,26 @@ const ProtectedModule = Component => {
 
     return <Component {...props} />
   }
+
+  // alternatively you could redirect the user to a login route as follows
+  // const Auth = props => {
+  //   // checks whether we are on client / browser or server.
+  //   if (typeof window !== 'undefined') {
+  //     const router = useRouter()
+
+  //     // If the user is not signed we redirect to "/" page.
+  //     if (!isSigned) {
+  //       router.replace('/')
+  //       return null
+  //     }
+
+  //     // If the user is signed we just render the component that was passed with all its props
+  //     return <Component {...props} />
+  //   }
+
+  //   // If we are on server, return null
+  //   return null
+  // }
 
   if (Component.getInitialProps) {
     Auth.getInitialProps = Component.getInitialProps
